6c092baa0665dbccd2607a86cbec8f24aedc8c35
johnbaby12 <durveshpatil151@gmail.com>
2025-11-03 09:11:46 +0530

    traversenode.c

---
+++ traversenode.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void appendNode(struct Node** head, int value) {
+    struct Node* newNode = createNode(value);
+    if (*head == NULL) {
+        *head = newNode;
+    } else {
+        struct Node* temp = *head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+
+void traverseList(struct Node* head) {
+    struct Node* temp = head;
+    printf("Linked List elements:\n");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+    int n, value, i;
+
+    clrscr();
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        printf("Enter value for node %d: ", i + 1);
+        scanf("%d", &value);
+        appendNode(&head, value);
+    }
+
+    traverseList(head);
+
+    getch();
+    return 0;
+}
